408b44372233149215eeef127a85c5ac9c33825a
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-03-01 14:52:07 +0100

    Add ESLint configuration, GitHub workflows for build, code quality, and tests, and set up Husky for pre-commit hooks

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['no-commented-code'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-commented-code/no-commented-code': 'warn',
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.types.ts',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+};
