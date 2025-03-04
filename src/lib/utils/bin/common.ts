3a19807e22dfcca367a18f2427a214c02728b5a0
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-03-04 13:32:44 +0100

    Fix import path for package.json in common utility module

--- src/lib/utils/bin/common.ts
+++ src/lib/utils/bin/common.ts
@@ -1,5 +1,5 @@
 export const accountCredentialsEnvironmentKey = 'GOOGLE_APPLICATION_CREDENTIALS';
 export const defaultaccountCredentialsPath = 'firebase-export.json';
 
-import packageInfo from '../../../package.json';
+import packageInfo from 'package.json';
 export { packageInfo };
