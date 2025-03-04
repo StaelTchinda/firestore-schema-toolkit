496f4cc6a8091387ab34265b3f5e13c23340d316
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-03-04 17:51:44 +0100

    Refactor validation error handling and move related functions to schema validation module

--- src/bin/firestore-schema/validate/command.ts
+++ src/bin/firestore-schema/validate/command.ts
@@ -13,42 +13,8 @@ import {
   initFirestore,
   getCollectionDocuments,
 } from "src/lib/utils/firestore";
-import { Ajv, ErrorObject, Schema, SchemaCxt } from "ajv";
-
-interface ErrorArrayObject extends ErrorObject {
-  instancePaths: string[];
-}
-
-export function mergeValidationErrors(
-  errors: ErrorObject[]
-): ErrorArrayObject[] {
-  // Merge errors with the same message, params, keyword, schemaPath. The instancePath are concatenated and collected in an array.
-  const mergedErrors: ErrorArrayObject[] = [];
-  for (const error of errors) {
-    const existingError = mergedErrors.find(
-      (e) =>
-        e.keyword === error.keyword &&
-        e.message === error.message &&
-        JSON.stringify(e.params) === JSON.stringify(error.params) &&
-        e.schemaPath === error.schemaPath
-    );
-    if (existingError) {
-      existingError.instancePaths.push(error.instancePath);
-      existingError.instancePath = existingError.instancePaths.join(",");
-    } else {
-      mergedErrors.push({ ...error, instancePaths: [error.instancePath] });
-    }
-  }
-
-  return mergedErrors;
-}
-
-export function mapItemSchemaToCollectionSchema(itemSchema: Schema): Schema {
-  return {
-    type: "array",
-    items: itemSchema,
-  };
-}
+import { Ajv, ErrorObject, Schema } from "ajv";
+import { mapItemSchemaToCollectionSchema, mergeValidationErrors } from "src/lib/schema/validate";
 
 export async function executeAsyncValidateCommand(
   program: Command
