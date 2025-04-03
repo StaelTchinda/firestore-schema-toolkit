dc3042ab3bb99fd324ccbdd5d870f223ec282b1a
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2025-04-03 16:33:05 +0200

    Refactor export command to improve document data handling and error management during schema generation

--- src/bin/firestore-schema/export/command.ts
+++ src/bin/firestore-schema/export/command.ts
@@ -6,6 +6,7 @@ import JsonSchemaGenrator from "json-schema-generator";
 import { parseParams, validateParams } from "src/bin/firestore-schema/export/params";
 import { isPathFolder } from "src/lib/utils/file";
 import { FirebaseCredentials, getCredentialsFromFile, initFirestore, getCollectionDocuments, getAllCollectionNames, parseNestedDocumentReferenceToSimpleObject } from "src/lib/utils/firestore";
+import { DocumentData } from "firebase-admin/firestore";
 
 export async function executeAsyncExportCommand(program: Command): Promise<void> {
   const params = parseParams(program);
@@ -29,18 +30,33 @@ export async function executeAsyncExportCommand(program: Command): Promise<void>
 
   for (const collectionName of collectionsToExport) {
     params.verbose && console.log(`Importing data for collection: ${collectionName}`);
-    const collectionData: unknown[] = await getCollectionDocuments(firestore, collectionName);
+    const collectionData: DocumentData[] = await getCollectionDocuments(firestore, collectionName);
+    params.verbose && console.log(`Fetched ${collectionData.length} documents from collection: ${collectionName} with keys: ${new Set(collectionData.map((doc) => Object.keys(doc || {})).flatMap((keys) => keys))}`);
 
     params.verbose && console.log(`Parsing nested document references for collection: ${collectionName}`);
-    const parsedCollectionData = parseNestedDocumentReferenceToSimpleObject(collectionData);
+    const parsedCollectionData = collectionData.map(parseNestedDocumentReferenceToSimpleObject);
+    if (!parsedCollectionData || parsedCollectionData.length === 0) {
+      console.warn(
+        `No documents found in collection: ${collectionName}. Skipping schema generation.`
+      );
+      continue;
+    }
     
     params.verbose && console.log(`Generating schema for collection: ${collectionName}`);
-    const collectionSchema = JsonSchemaGenrator(parsedCollectionData);
-    if (!collectionSchema || !collectionSchema["items"]) {
+    let collectionSchema;
+    try {
+      collectionSchema = JsonSchemaGenrator(parsedCollectionData);
+    } catch (error) {
+      console.error(`Error generating schema for collection: ${collectionName} using data`, error);
+      continue;
+    }
+    if (!collectionSchema) {
       console.warn(
         `Failed to generate schema for collection: ${collectionName}. Collection is empty or has no schema.`
       );
       continue;
+    } else {
+      params.verbose && console.log(`Schema generated for collection: ${collectionName}`);
     }
     const dataSchema = collectionSchema["items"];
 
